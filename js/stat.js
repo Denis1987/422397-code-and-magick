4e39eb864a63af8d19ef0a8abb183d59a885b9e1
Denis1987 <denis-a1987@mail.ru>
2017-11-25 21:07:31 +0300

    разобрался в коде и сделал имена игроков рядом со временем

--- js/stat.js
+++ js/stat.js
@@ -1,87 +1,74 @@
 "use strict";
 
 window.renderStatistics = function (ctx, names, times) {
-  roundedRect(ctx, 100, 10, 420, 270, 10);
+    roundedRect(ctx, 100, 10, 420, 270, 10);
 
 
-  console.log(names);
+    console.log(names);
 
-  ctx.fillStyle = '#000';
-  ctx.font = '14px PT Mono';
+    ctx.fillStyle = '#000';
+    ctx.font = '14px PT Mono';
 
-  ctx.fillText('Ура вы победили!', 120, 40);
-  //    не понятно что за цифры выше после текста
+    ctx.fillText('Ура вы победили!', 120, 40);
 
-  var max = -1;
-  var maxIndex = -1;
-  //    не понятно почему переменная max именно -1 а не 0 и почему они равны
+    var max = -1;
+    var maxIndex = -1;
 
 
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
+    for (var i = 0; i < times.length; i++) {
+        var time = times[i];
+        if (time > max) {
+            max = time;
+        }
     }
-  }
-  console.log(times);
-  //    что такое for? потом объявляется переменна i а вот потом после точки с запятой что это?
-  //        переменная time равна одному из значений массива times а потом если это значение больше мексимального которое равно -1 то максимальное и будет равно time которое равно одному из значений times?
+    var histogramWidth = 150;
+    var step = histogramWidth / (max - 0);
 
 
-  var histogramWidth = 150;
-  var step = histogramWidth / (max - 0);
-  //    почему именно ширина? как задать высоту и сделать так чтобы они расли в высотуц?
+    var barWidth = 60;
+    var indent = 80;
+    var initialX = 100;
+    var initialY = 80;
+    var lineHeight = 15;
 
+    for (var i = 0; i < times.length; i++) {
+        ctx.fillText(Math.round(times[i] / 1000), initialX + barWidth + indent * i, initialY - 10);
+        ctx.fillText(names[i], initialX + (barWidth + 10) + indent * i, initialY - 10);
+    }
+    for (var i = 0; i < times.length; i++) {
+        if (names[i] === 'Вы') {
+            ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+            ctx.globalAlpha = 1;
+        } else {
+            ctx.fillStyle = 'blue';
+            ctx.globalAlpha = Math.random();
 
-  ctx.fillText('Худшее время: ' + max, 120, 60);
-
-  //и эти цифры тоже
-
-  ctx.fillRect(120, 100 + max * step - times[0] * step, 20, times[0] * step);
-  ctx.fillRect(160, 100 + max * step - times[1] * step, 20, times[1] * step);
-  ctx.fillRect(200, 100 + max * step - times[2] * step, 20, times[2] * step);
-  //    ctx.fillRect(x, y, width, height);
-  ctx.fillRect(240, 100 + max * step - times[3] * step, 20, times[3] * step);
-
-
-  //
-  var barWidth = 20;
-  //
-  var indent = 40;
-  var initialX = 80;
-  var initialY = 120;
-  //        //    почему 80? ведь они на разном расстоянии друг от друга
-  var lineHeight = 15;
-  //
-  for (var i = 0; i < times.length; i++) {
-    //        ctx.fillRect(initialY);
-    ctx.fillText(names[i], initialY + initialY, initialX + lineHeight + indent * i);
-  }
-
+        }
+        ctx.fillRect(initialX + barWidth + indent * i, initialY, barWidth, times[i] * step, names);
+    }
 
 };
 
-
 function roundedRect(ctx, x, y, width, height, radius) {
-  ctx.fillStyle = '#fff';
-  ctx.beginPath();
-  ctx.moveTo(x, y + radius);
-  ctx.lineTo(x, y + height - radius);
-  ctx.quadraticCurveTo(x, y + height, x + radius, y + height);
-  ctx.lineTo(x + width - radius, y + height);
-  ctx.quadraticCurveTo(x + width, y + height, x + width, y + height - radius);
-  ctx.lineTo(x + width, y + radius);
-  ctx.quadraticCurveTo(x + width, y, x + width - radius, y);
-  ctx.lineTo(x + radius, y);
-  ctx.quadraticCurveTo(x, y, x, y + radius);
-  ctx.shadowColor = "rgba(0, 0, 0, 0.7)";
-  //    ctx.shadowBlur = 20;
-  ctx.shadowOffsetX = 10;
-  ctx.shadowOffsetY = 10;
+    ctx.fillStyle = '#fff';
+    ctx.beginPath();
+    ctx.moveTo(x, y + radius);
+    ctx.lineTo(x, y + height - radius);
+    ctx.quadraticCurveTo(x, y + height, x + radius, y + height);
+    ctx.lineTo(x + width - radius, y + height);
+    ctx.quadraticCurveTo(x + width, y + height, x + width, y + height - radius);
+    ctx.lineTo(x + width, y + radius);
+    ctx.quadraticCurveTo(x + width, y, x + width - radius, y);
+    ctx.lineTo(x + radius, y);
+    ctx.quadraticCurveTo(x, y, x, y + radius);
+    ctx.shadowColor = "rgba(0, 0, 0, 0.7)";
+    ctx.shadowOffsetX = 10;
+    ctx.shadowOffsetY = 10;
 
-  ctx.closePath();
-  ctx.fill();
-  ctx.shadowColor = 0;
-  ctx.shadowOffsetX = 0;
-  ctx.shadowOffsetY = 0;
+
+    ctx.closePath();
+    ctx.fill();
+    ctx.shadowColor = 0;
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 0;
 }
